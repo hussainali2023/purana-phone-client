65b841e4095f734d2f9391c548dc81328f102898
hussain2023 <iamhussainali2@gmail.com>
2022-11-24 09:26:46 +0530

    successfully design the login and sign up Page

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
         <Link
           to="/login"
           className="inline-flex items-center justify-center w-full h-12 px-6 tracking-wide text-white transition duration-200 rounded shadow-md bg-yellow-400 hover:bg-yellow-500"
-          aria-label="Logi"
+          aria-label="Login"
           title="Login"
         >
           Login
@@ -27,31 +27,13 @@ const Navbar = () => {
     <div className="bg-blue-500">
       <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div className="relative flex items-center justify-between">
-          <a
-            href="/"
-            aria-label="Company"
-            title="Company"
-            className="inline-flex items-center"
-          >
-            <svg
-              className="w-8 text-teal-accent-400"
-              viewBox="0 0 24 24"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeMiterlimit="10"
-              stroke="currentColor"
-              fill="none"
-            >
-              <rect x="3" y="1" width="7" height="12" />
-              <rect x="3" y="17" width="7" height="6" />
-              <rect x="14" y="1" width="7" height="6" />
-              <rect x="14" y="11" width="7" height="12" />
-            </svg>
-            <span className="ml-2 text-xl font-extrabold tracking-wide text-yellow-300 uppercase">
-              purana phone
-            </span>
-          </a>
+          <Link to="/" className="inline-flex items-center">
+            <div className="ml-2  text-xl font-extrabold tracking-wide text-yellow-300 uppercase">
+              <span className=" leading-none">Purana</span>
+              <br />
+              <span className="leading-none">Phone</span>
+            </div>
+          </Link>
           <ul className="flex items-center hidden space-x-8 lg:flex text-white font-medium">
             {listItems}
           </ul>
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,11 +1,118 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+  const {
+    register,
+    formState: { error },
+    handleSubmit,
+  } = useForm();
+
+  const handleLogin = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <h1>This is SignUp Page</h1>
+      <section className="h-screen">
+        <div className="container px-6 py-12 h-full">
+          <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+            <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+                className="w-full"
+                alt="/"
+              />
+            </div>
+            <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
+              <h1 className=" text-center text-blue-700 font-bold text-3xl mb-6">
+                Please Login
+              </h1>
+              <form onSubmit={handleSubmit(handleLogin)}>
+                <div className="mb-6">
+                  <label
+                    className=" flex justify-start text-lg mb-2"
+                    htmlFor="name"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    {...register("name", { required: "Name is Required" })}
+                    name="name"
+                    className="form-control block w-full px-4 py-2 text-lg font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    placeholder="Your Name"
+                  />
+                </div>
+                <div className="mb-6">
+                  <label
+                    className=" flex justify-start text-lg mb-2"
+                    htmlFor="email"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    {...register("email", {
+                      required: "Email Address is required",
+                    })}
+                    name="email"
+                    className="form-control block w-full px-4 py-2 text-lg font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    placeholder="Email address"
+                  />
+                </div>
+                <label
+                  className=" flex justify-start text-lg mb-2"
+                  htmlFor="name"
+                >
+                  What type of User You are ?
+                </label>
+                <select
+                  name="typeOfUser"
+                  {...register("typeOfUser", {
+                    required: "Password is Required",
+                  })}
+                  id=""
+                  className=" mb-4 w-full px-4 py-2 text-lg font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                >
+                  <option value="buyer">Buyer</option>
+                  <option value="seller">Seller</option>
+                </select>
+                <div className="mb-3">
+                  <label
+                    className=" flex justify-start text-lg mb-2"
+                    htmlFor="name"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    {...register("password")}
+                    name="password"
+                    className="form-control block w-full px-4 py-2 text-lg font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="flex justify-end mb-2">
+                  <a
+                    href="#/"
+                    className="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+                <button
+                  type="submit"
+                  className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+                >
+                  Sign Up
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
-
 export default SignUp;
