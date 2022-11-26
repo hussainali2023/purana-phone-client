8c635a42e605baa72e506fbed378666483b0ec96
hussain2023 <iamhussainali2@gmail.com>
2022-11-26 15:08:03 +0530

    show my products for seller in dashboard

--- src/Pages/Dashboard/AddProduct.js
+++ src/Pages/Dashboard/AddProduct.js
@@ -2,8 +2,68 @@ import React from "react";
 
 const AddProduct = () => {
   return (
-    <div>
-      <h1 className=" text-5xl"> This is Add product Section</h1>
+    <div className="grid grid-cols-2">
+      <div className=" ml-4">
+        <form action="">
+          <div class="form-group mb-6">
+            <label
+              for="exampleInputEmail2"
+              class="form-label inline-block mb-2 text-gray-700"
+            >
+              Email address
+            </label>
+            <input
+              type="email"
+              class="form-control
+        block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleInputEmail2"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+          </div>
+          <div class="form-group mb-6">
+            <label
+              for="exampleInputPassword2"
+              class="form-label inline-block mb-2 text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              class="form-control block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleInputPassword2"
+              placeholder="Password"
+            />
+          </div>
+        </form>
+      </div>
+      <div>product section</div>
     </div>
   );
 };
--- src/Pages/Dashboard/MyProduct.js
+++ src/Pages/Dashboard/MyProduct.js
@@ -1,9 +1,66 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider";
 
 const MyProduct = () => {
+  const { user } = useContext(AuthContext);
+  const {
+    data: products = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ["products", user.email],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/products?email=${user.email}`
+      );
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <p>Loading.....</p>;
+  }
   return (
     <div>
-      <h1 className=" text-7xl">This is My product</h1>
+      <h3 className="text-3xl mb-5">My Products</h3>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Photo</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Status</th>
+              <th>Ads</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products &&
+              products?.map((product, i) => (
+                <tr key={product._id}>
+                  <th>{i + 1}</th>
+                  <td>
+                    <img src={product.photo} className="h-20" alt="" />
+                  </td>
+                  <td>{product.phoneName}</td>
+                  <td>{product.salePrice}</td>
+                  <td></td>
+                  <td>
+                    <button>Advertise</button>
+                  </td>
+                  <td>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
